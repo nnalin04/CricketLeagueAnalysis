c301867abf7cf86fba3088201fbd0d6787deb260
nnalin04 <nnalin04@gmail.com>
2020-08-27 00:42:10 +0530

    Added test to get the Cricketers for best Striking Rate with 6s & 4s

--- src/main/java/cricketleagueanalysis/CricketLeagueAnalyser.java
+++ src/main/java/cricketleagueanalysis/CricketLeagueAnalyser.java
@@ -16,45 +16,54 @@ public class CricketLeagueAnalyser {
         return factSheetMap;
     }
 
-    public String getBattingAvrageSortedFactSheet() {
-        Comparator<FactSheetDAO> censusComparator = Comparator.comparing(leagueFact -> leagueFact.avgRun);
+    public String getBattingAverageSortedFactSheet() {
+        Comparator<FactSheetDAO> avgRunComparator = Comparator.comparing(leagueFact -> leagueFact.avgRun);
         List<FactSheetDAO> factSheetDAO = factSheetMap.values().stream()
                 .collect(Collectors.toList());
-        this.sort(factSheetDAO, censusComparator);
+        this.sort(factSheetDAO, avgRunComparator);
         String sortedStateCensusJson = new Gson().toJson(factSheetDAO);
         return sortedStateCensusJson;
     }
 
     public String getStrikingRatesSortedFactSheet() {
-        Comparator<FactSheetDAO> censusComparator = Comparator.comparing(leagueFact -> leagueFact.strikeRate);
+        Comparator<FactSheetDAO> strikeRateComparator = Comparator.comparing(leagueFact -> leagueFact.strikeRate);
         List<FactSheetDAO> factSheetDAO = factSheetMap.values().stream()
                 .collect(Collectors.toList());
-        this.sort(factSheetDAO, censusComparator);
+        this.sort(factSheetDAO, strikeRateComparator);
         String sortedStateCensusJson = new Gson().toJson(factSheetDAO);
         return sortedStateCensusJson;
     }
 
-    public String getMax6sAnd4sSortedFactSheet() {
-        Comparator<FactSheetDAO> censusComparator = Comparator.comparing(leagueFact -> leagueFact.fours * 4 + leagueFact.sixes * 6);
+    public String getMax4sAnd6sSortedFactSheet() {
+        Comparator<FactSheetDAO> fourAndSixComparator = Comparator.comparing(leagueFact -> leagueFact.fours * 4 + leagueFact.sixes * 6);
         List<FactSheetDAO> factSheetDAO = factSheetMap.values().stream()
                 .collect(Collectors.toList());
-        this.sort(factSheetDAO, censusComparator);
+        this.sort(factSheetDAO, fourAndSixComparator);
         String sortedStateCensusJson = new Gson().toJson(factSheetDAO);
         return sortedStateCensusJson;
     }
 
-    private void sort(List<FactSheetDAO> factSheetDAOS, Comparator<FactSheetDAO> censusComparator) {
+    public String getMax4sAnd6sWithStrikeRatesSortedFactSheet() {
+        Comparator<FactSheetDAO> strikeRateComparator = Comparator.comparing(leagueFact -> leagueFact.strikeRate);
+        Comparator<FactSheetDAO> fourAndSixComparator = Comparator.comparing
+                (leagueFact -> leagueFact.fours * 4 + leagueFact.sixes * 6);
+        List<FactSheetDAO> factSheetDAO = factSheetMap.values().stream()
+                .collect(Collectors.toList());
+        this.sort(factSheetDAO, fourAndSixComparator.thenComparing(strikeRateComparator));
+        String sortedStateCensusJson = new Gson().toJson(factSheetDAO);
+        return sortedStateCensusJson;
+    }
+
+    private void sort(List<FactSheetDAO> factSheetDAOS, Comparator<FactSheetDAO> comparator) {
         for (int i = 0; i < factSheetDAOS.size()-1; i++) {
             for (int j =0; j< factSheetDAOS.size() -i -1; j++) {
                 FactSheetDAO leagueFact1 = factSheetDAOS.get(j);
                 FactSheetDAO leagueFact2 = factSheetDAOS.get(j+1);
-                if (censusComparator.compare(leagueFact1, leagueFact2) < 0){
+                if (comparator.compare(leagueFact1, leagueFact2) < 0){
                     factSheetDAOS.set(j, leagueFact2);
                     factSheetDAOS.set(j+1, leagueFact1);
                 }
             }
         }
     }
-
-
 }
